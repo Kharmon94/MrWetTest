9996e79f458ca42dbcaccfd9644889939bf2842b
Kharmon <Kyleharmon52@gmail.com>
2025-03-16 23:09:01 -0400

    refactor test concept: views 3

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,9 @@
 class User < ApplicationRecord
   rolify
 
-  has_many :assessments, dependent: :destroy
+  has_many :test_attempts, dependent: :destroy
+
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
