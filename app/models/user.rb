64c9f3a333883bc6b28be2c34212432431f8e5a8
Kharmon <Kyleharmon52@gmail.com>
2025-10-14 22:22:06 -0400

    mobile responsiveness & user settings

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,9 @@ class User < ApplicationRecord
   has_many :assessments, dependent: :destroy
   has_many :payments, dependent: :destroy
 
+  # Active Storage attachments
+  has_one_attached :avatar
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
